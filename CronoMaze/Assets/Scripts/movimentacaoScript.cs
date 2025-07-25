cdd083690a8d2597f18481607f0197082aaf9925
Mary <marisangelalima110@gmail.com>
2025-07-25 11:03:50 -0300

    feat: barra de vida

--- CronoMaze/Assets/Scripts/movimentacaoScript.cs
+++ CronoMaze/Assets/Scripts/movimentacaoScript.cs
@@ -1,50 +1,54 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class movimentacaoScript : MonoBehaviour
 {
     public float velocidade = 0.05f;
     public Transform cameraTransform;
     private Rigidbody rb;
-    private bool morreu = false;
+    private bool caiu = false;
+    private statusJogadorScript status;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        status = GetComponent<statusJogadorScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow)){
+        if(Input.GetKey(KeyCode.A)){
             // this.gameObject.transform.position = this.gameObject.transform.position + Vector3.left * velocidade;
             this.gameObject.transform.Rotate(Vector3.up,-1f);
         }
-        if(Input.GetKey(KeyCode.RightArrow)){
+        if(Input.GetKey(KeyCode.D)){
             // this.gameObject.transform.position = this.gameObject.transform.position + Vector3.right * velocidade;
             this.gameObject.transform.Rotate(Vector3.up,1f);
         }
         Vector3 cameraForward = cameraTransform.forward;
         cameraForward.y = 0;
         cameraForward.Normalize();
-        if(Input.GetKey(KeyCode.DownArrow)){
+        if(Input.GetKey(KeyCode.S)){
             // this.gameObject.transform.position = this.gameObject.transform.position + this.gameObject.transform.forward * -1f* velocidade;
             // this.gameObject.transform.position += Vector3.back * velocidade;
             transform.position -= cameraForward * velocidade;
         }
-        if(Input.GetKey(KeyCode.UpArrow)){
+        if(Input.GetKey(KeyCode.W)){
             // this.gameObject.transform.position = this.gameObject.transform.position + this.gameObject.transform.forward * velocidade;
             // this.gameObject.transform.position += Vector3.forward * velocidade; 
             transform.position += cameraForward * velocidade;
         }
-        if(morreu == false && transform.position.y < 0){
-            Debug.Log("Morreu");
-            morreu = true;
+        if(caiu = false && transform.position.y < 0){
+            status.removeVida();
+            caiu = true;
         }
     }
 
     void OnCollisionEnter(Collision other) {
         if(other.gameObject.tag.Equals("Enemy")){
             rb.AddForce(Vector3.back*700);
+            status.removeVida();
         }
         if(other.gameObject.tag.Equals("Fence") || other.gameObject.tag.Equals("Pillar")){
             rb.AddForce(Vector3.back*100);
---
+++ CronoMaze/Assets/Scripts/statusJogadorScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class statusJogadorScript : MonoBehaviour
+{
+    public Image barraVida;
+    public TextMeshProUGUI vidaJogadorTexto;
+    public int maximoVidas = 100;
+    int vidasAtual;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.vidasAtual = this.maximoVidas;
+        AtualizarUI();
+    }
+
+    public void removeVida() {
+        this.vidasAtual = this.vidasAtual - 2;
+        this.vidasAtual = Mathf.Clamp(vidasAtual, 0, maximoVidas);
+        AtualizarUI();
+        if(this.vidasAtual == 0){
+            SceneManager.LoadScene("morreuScene");
+        }
+    }
+
+    void AtualizarUI()
+    {
+        if(vidaJogadorTexto != null){
+            vidaJogadorTexto.text = this.vidasAtual.ToString();
+        }
+        if(barraVida != null){
+            barraVida.fillAmount = (float)this.vidasAtual / maximoVidas;
+        }
+    }
+}
